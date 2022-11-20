b61a77d2bc6a68c6ad1440d223158574e19811e6
Bilal Mülayim <bilalmulayim@Bilal-MBP.home>
2022-11-20 03:36:02 +0300

    1.40.00

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,37 @@
 import React, { useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import { Login, Home } from "./components/index"
+import { Login, Home } from "./components/index";
 import { useState } from "react";
 import { app } from "./config/firebase.config";
 import { getAuth } from "firebase/auth";
 
 const App = () => {
+  const firebaseAuth = getAuth(app);
+  const navigate = useNavigate();
 
-  const firebaseAuth = getAuth(app)
-  const navigate = useNavigate()
-  
-  const [auth, setauth] = useState(false ||  window.localStorage.getItem("auth") === "true")
+  const [auth, setAuth] = useState(
+    false || window.localStorage.getItem("auth") === "true"
+  );
 
   useEffect(() => {
     firebaseAuth.onAuthStateChanged((userCred) => {
-      console.log(userCred)
-    })
-  },[])
+      if (userCred) {
+        userCred.getIdToken().then((token) => {
+          console.log(token);
+        });
+      } else {
+        setAuth(false);
+        window.localStorage.setItem("auth", "false");
+        navigate("/Login");
+      }
+    });
+  }, []);
 
   return (
     <div>
       <Routes>
-        <Route path="/Login" element={<Login/>}/>
-        <Route path="/*" element={<Home/>}/>
+        <Route path="/Login" element={<Login setAuth={setAuth} />} />
+        <Route path="/*" element={<Home />} />
       </Routes>
     </div>
   );
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,15 +6,29 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 
 
-const Login = () => {
+const Login = ({setAuth}) => {
     const firebaseAuth = getAuth(app)
     const provider = new GoogleAuthProvider()
     const navigate = useNavigate()
     const LoginWithGoogle = async() => {
         await signInWithPopup(firebaseAuth, provider).then((userCred) => {
-            console.log(userCred)
+            if(userCred){
+                setAuth(true)
+                window.localStorage.setItem("auth", "true")
+                firebaseAuth.onAuthStateChanged((userCred) => {
+                    if(userCred){
+                        userCred.getIdToken().then((token) => {
+                            console.log(userCred)
+                        })
+                        
+                        navigate("/" ,{replace : true})
+                    }else{
+                    setAuth(false)
+                    navigate("/Login")
+                    }
+                })
+            }
         })
-
     }
   return (
     <div className=' relative w-screen h-screen'>
