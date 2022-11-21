1615780ef49787cc9e941e4599b5aeaa7c638aa2
Bilal Mülayim <bilalmulayim@192.168.1.47>
2022-11-21 17:31:41 +0300

    first video completed

--- server/app.js
+++ server/app.js
@@ -11,9 +11,15 @@ app.get("/", (req, res) => {
     return res.json("hi there...")
 })
 
+//user authentication route
+const userRoute = require("./routes/auth")
+app.use("/api/users", userRoute)
+
 mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true });
 mongoose.connection
   .once("open", () => console.log("Connected"))
   .on("error", (error) => {
     console.log(`Error : ${error}`);
   });
+
+app.listen(4000, () => console.log("lisitening to port 4000"));
