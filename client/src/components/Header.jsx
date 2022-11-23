1b3f2780a4e08f03732042d10f80a3c4e9145ecd
BilalMlym <bilalmlym@gmail.com>
2022-11-23 15:09:48 +0300

    55 min second lesson

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Logo} from "../assets/img/index" 
+import {Logo , Avatar} from "../assets/img/index" 
 import {NavLink} from "react-router-dom"
+import {FaCrown } from "react-icons/fa"
 
 import { isActiveStyles , isNotActiveStyles} from '../utils/styles'
 
@@ -18,6 +19,13 @@ const Header = () => {
         <li className="mx-5 text-lg"><NavLink to={"./Premium"} className ={({isActive}) => isActive ? isActiveStyles : isNotActiveStyles}>Premium</NavLink></li>
         <li className="mx-5 text-lg"><NavLink to={"./ContactUs"} className ={({isActive}) => isActive ? isActiveStyles : isNotActiveStyles}>Contact Us</NavLink></li>
       </ul>
+      <div className="flex items-center ml-auto cursor-pointer gap-2 relative">
+        <img src={Avatar} alt="UserPic" className='w-12 min-w-[44px] object-cover rounded-full shadow-lg'/>
+        <div className='flex flex-col'>
+          <p className='text-textColor text-lg hover:text-headingColor font-semibold'>UserName</p>
+          <p className='flex items-center gap-2 text-xs text-gray-500 font-normal'>Premium Memmber <FaCrown className='text-sm -ml-1 text-yellow-500' /></p>
+        </div>
+      </div>
     </header>
   )
 }
