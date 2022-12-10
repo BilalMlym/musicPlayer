ae3578820a2326be0d40a6da1af96a604d0c0fec
Bilal Mülayim <bilalmulayim@Bilal-MacBook-Pro.local>
2022-12-10 12:45:56 +0300

    40 minutes fourth lesson

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -41,3 +41,6 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+
+//
--- client/src/components/DashboardHome.jsx
+++ client/src/components/DashboardHome.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+
+export const DashboardCard = ({icon, name, count}) => {
+  return (
+    <div className='p-4 w-40 gap-3 h-auto rounded-lg shadow-md bg-red-400'>
+      {icon}
+      <p className='text-xl text-textColor font-semibold'>{name} </p>
+      <p className='text-xl text-textColor font-semibold'>{count} </p>
+    </div>
+  )
+}
+
 function DashboardHome() {
   return (
-    <div>DashboardHome A HOSGELDİNİZ</div>
+    <div className='w-full flex items-center p-6 justify-evenly flex-wrap'>
+      <DashboardCard />
+      <DashboardCard />
+      <DashboardCard />
+      <DashboardCard />
+    </div>
   )
 }
 
