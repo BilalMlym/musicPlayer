195b1c9548bad1c7c892e3c46503d708724d6916
Bilal Mülayim <bilalmulayim@Bilal-MBP.home>
2022-12-11 10:43:53 +0300

    lesson 4.2 15.00

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { IoHome } from "react-icons/io5";
 import { NavLink, Route, Routes } from "react-router-dom";
-import { DashboardNewSong, DashboardUsers } from ".";
+import { DashboardNewSong } from ".";
 import { isActiveStyles, isNotActiveStyles } from "../utils/styles";
 import DashboardAlbum from "./DashboardAlbum";
 import DashboardArtist from "./DashboardArtist";
-import DashboardHome from "./DashboardHome";
+import DashBoardHome from "./DashBoardHome";
 import DashboardSongs from "./DashboardSongs";
-import DashboardUser from "./DashboardUsers";
+import DashboardUser from "./DashboardUser";
 import Header from "./Header";
 
 const Dashboard = () => {
@@ -16,19 +16,24 @@ const Dashboard = () => {
       <Header />
 
       <div className="w-[60%] my-2 p-4 flex items-center justify-evenly">
+        {/* prettier-ignore */}
         <NavLink to={"/dashboard/home"}><IoHome className="text-2xl text-textColor" /></NavLink>
-        <NavLink to={"/dashboard/users"} className={({ isActive }) => isActive ? isActiveStyles : isNotActiveStyles }> Users </NavLink>
+        {/* prettier-ignore */}
+        <NavLink to={"/dashboard/user"} className={({ isActive }) => isActive ? isActiveStyles : isNotActiveStyles }> Users </NavLink>
 
+        {/* prettier-ignore */}
         <NavLink to={"/dashboard/songs"} className={({ isActive }) => isActive ? isActiveStyles : isNotActiveStyles }> Songs </NavLink>
 
+        {/* prettier-ignore */}
         <NavLink to={"/dashboard/artist"} className={({ isActive }) => isActive ? isActiveStyles : isNotActiveStyles }> Artist </NavLink>
 
+        {/* prettier-ignore */}
         <NavLink to={"/dashboard/albums"} className={({ isActive }) => isActive ? isActiveStyles : isNotActiveStyles }> Albums </NavLink>
       </div>
 
       <div className="my-4 w-full p-4">
         <Routes>
-          <Route path="/home" element={<DashboardHome />} />
+          <Route path="/home" element={<DashBoardHome />} />
           <Route path="/user" element={<DashboardUser />} />
           <Route path="/songs" element={<DashboardSongs />} />
           <Route path="/artist" element={<DashboardArtist />} />
@@ -41,6 +46,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-
-//
--- client/src/components/DashboardUsers.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DashboardUsers() {
-  return (
-    <div>DashboardUsers</div>
-  )
-}
-
-export default DashboardUsers
