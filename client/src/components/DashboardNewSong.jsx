81411305484316b32d0093f8b673fe1f4f5ef838
Bilal Mülayim <bilalmulayim@Bilal-MBP.home>
2022-12-17 20:14:05 +0300

    5 lesson 1.30.00

--- client/src/components/DashboardNewSong.jsx
+++ client/src/components/DashboardNewSong.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import {
+  getStorage,
+  ref,
+  getDownloadURL,
+  uploadBytesResumable,
+  deleteObject,
+} from "firebase/storage";
+import { motion } from "framer-motion";
+
+import { BiCloudUpload } from "react-icons/bi";
+import { MdDelete } from "react-icons/md";
+
+import { storage } from "../config/firebase.config";
+import { useStateValue } from "../Context/StateProvider";
+import FilterButtons from "./FilterButtons";
+import {
+  getAllAlbums,
+  getAllArtist,
+  getAllSongs,
+  saveNewAlbum,
+  saveNewArtist,
+  saveNewSong,
+} from "../api";
+import { actionType } from "../Context/reducer";
+import { filterByLanguage, filters } from "../utils/supportFunctions";
+import { IoMusicalNote } from "react-icons/io5";
+import AlertSuccess from "./AlertSuccess";
+import AlertError from "./AlertError";
 
 function DashboardNewSong() {
+  const [songName, setsongName] = useState("");
   return (
-    <div>DashboardNewSong</div>
-  )
+    <div className="flex flex-col items-center justify-center p-4 border border-gray-300 rounded-md gap-4">
+      <input
+        type="text"
+        placeholder="type song name..."
+        className="w-full p-3 rounded-md text-base font-semibold text-textColor outline-none shadow-sm border borger-gray-300 bg-transparent"
+        value={songName}
+        onChange={(e) => setsongName(e.target.value)}
+      />
+      <div className="flex w-full justify-between flex-wrap items-center gap-4">
+        <FilterButtons filterData={""} flag={"Artist"} />
+        <FilterButtons filterData={""} flag={"Albums"}></FilterButtons>
+        <FilterButtons filterData={""} flag={"Languages"}></FilterButtons>
+        <FilterButtons filterData={""} flag={"Category"}></FilterButtons>
+      </div>
+    </div>
+  );
 }
 
-export default DashboardNewSong
+export default DashboardNewSong;
---
+++ client/src/components/FilterButtons.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FilterButtons() {
+  return (
+    <div className='border border-gray-300 rounded-md px-4 py-1 relative cursor-pointer hover:hover-gray-700'>FilterButtons</div>
+  )
+}
+
+export default FilterButtons
